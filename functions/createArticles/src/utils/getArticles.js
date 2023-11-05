f0887ac8de2933c8a4195c7733cf217cd0f082c5
wasswaenockmale <enockmale97@gmail.com>
2023-11-05 16:02:58 +0300

    Retrieve articles from the REST API

---
+++ functions/createArticles/src/utils/getArticles.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+// Get the articles.
+export default async function getArticles() {
+  try {
+    return await axios
+      .get(process.env.TECH_IN_AFRICA)
+      .then(response => {
+        return response.data;
+      })
+  } catch (error) {
+    console.log("Error in fetching the articles",error)
+  }
+}
