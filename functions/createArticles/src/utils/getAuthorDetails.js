e6cc5e77ae79b85c32d43a3940cac00dbc6aaee9
wasswaenockmale <enockmale97@gmail.com>
2023-11-06 12:06:25 +0300

    function to return author information

--- functions/createArticles/src/utils/getAuthorDetails.js
+++ functions/createArticles/src/utils/getAuthorDetails.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-export default async function getAuthorDetails() {
+export default async function getAuthorDetails(authorID) {
   try {
-      await axios
-    .get(process.env.AUTHOR_ENDPOINT)
-    .then(response => {
-      console.log(response.data)
-      return response.data
-    })
-    .catch(error => {
-      console.log("Error has occured", error);
-    })
+    await axios
+      .get(`${process.env.AUTHOR_ENDPOINT}/${authorID}`)
+      .then(response => {
+        // console.log(response.data)
+        return response.data
+      })
+      .catch(error => {
+        console.log("Error has occured", error);
+      })
   } catch (error) {
     console.log("There is an error", error);
   }
