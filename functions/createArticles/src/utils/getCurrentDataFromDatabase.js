48ff6096eca576b815891ce5900e3753e73a8e68
wasswaenockmale <enockmale97@gmail.com>
2023-11-08 15:41:20 +0300

    bug: function returning an array of Promises fixed:

--- functions/createArticles/src/utils/getCurrentDataFromDatabase.js
+++ functions/createArticles/src/utils/getCurrentDataFromDatabase.js
@@ -1,27 +1,16 @@
 import { Databases } from "node-appwrite";
 
 export default async function getCurrentDataFromDatabase(client) {
-  // database.
   const database = new Databases(client);
   try {
-    // get the documents.
-    console.log(process.env.APPWRITE_ARTICLES_COLLECTION_ID);
-    const data = await database.listDocuments(
+    const res = await database.listDocuments(
       process.env.APPWRITE_DATABASE_ID,
-      process.env.APPWRITE_ARTICLES_COLLECTION_ID,
+      process.env.APPWRITE_ARTICLES_COLLECTION_ID
     )
-      .then(response => response.documents)
-      .catch(error => {
-        console.log("Error in fetching data from the database", error);
-      })
-    
-    if (data) {
-      return data.map(async (rowData) => {
-        return rowData.articleID
-      })
-    }
-    return []
+    const articleIDs = res.documents.map(rowData => rowData.articleID);
+    return articleIDs;
   } catch (error) {
-    console.log("Get current data error", error);
+    console.log("Error in fetching data from the database", error);
+    throw error; // You might want to propagate the error
   }
 }
