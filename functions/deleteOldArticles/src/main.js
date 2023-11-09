5708360b5dcff04b0508fed4ec25e5cde6227369
wasswaenockmale <enockmale97@gmail.com>
2023-11-09 11:02:36 +0300

    Function to delete articles older than a week.

---
+++ functions/deleteOldArticles/src/main.js
@@ -0,0 +1,23 @@
+import { Client } from 'node-appwrite';
+import deleteArticles from './utils/deleteArticles';
+
+// This is your Appwrite function
+// It's executed each time we get a request
+export default async ({ req, res, log, error }) => {
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_PROJECT_ENDPOINT)
+    .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+
+  try {
+    // delete articles older than a week.
+    await deleteArticles(client);
+  } catch (error) {
+    console.log("Failed to delete articles older than a week.", error.message);
+  }
+
+  // `res.json()` is a handy helper for sending JSON
+  return res.json({
+    success: 'ok',
+  });
+};
