9012dc20bfff9830247e68d73ca50556eaba0cf1
wasswaenockmale <enockmale97@gmail.com>
2023-11-16 17:02:22 +0300

    added pagination to the request for articles

--- functions/createArticles/src/utils/getArticles.js
+++ functions/createArticles/src/utils/getArticles.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default async function getArticles() {
   try {
     return await axios
-      .get(process.env.TECH_IN_AFRICA)
+      .get(`${process.env.TECH_IN_AFRICA}?page=1&per_page=20`)
       .then(response => {
         return response.data;
       })
