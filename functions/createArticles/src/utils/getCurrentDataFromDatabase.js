a009e65eeccd91e42bb6cc8c7f85fce16aabce3d
wasswaenockmale <enockmale97@gmail.com>
2023-11-23 11:25:03 +0300

    chore: added pagination

--- functions/createArticles/src/utils/getCurrentDataFromDatabase.js
+++ functions/createArticles/src/utils/getCurrentDataFromDatabase.js
@@ -1,11 +1,15 @@
-import { Databases } from "node-appwrite";
+import { Databases, Query } from "node-appwrite";
 
 export default async function getCurrentDataFromDatabase(client) {
   const database = new Databases(client);
   try {
     const res = await database.listDocuments(
       process.env.APPWRITE_DATABASE_ID,
-      process.env.APPWRITE_ARTICLES_COLLECTION_ID
+      process.env.APPWRITE_ARTICLES_COLLECTION_ID,
+      [
+        Query.limit(50),
+        Query.offset(50)
+      ]
     )
     const articleIDs = res.documents.map(rowData => rowData.articleID);
     return articleIDs;
